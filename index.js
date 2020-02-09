27f7b6ad8d6b1df3e9987dc3ad753c91dc174c6a
wahyu-handayani <handayaniwahyu093@gmail.com>
2020-02-09 16:46:17 +0700

    allow cors

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-app.use(bodyParser.json());
-const conn = require('./src/config/config');
+const cors=require('cors')
+app.options('*', cors())
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
 
-const dataEngineer=require('./src/routes/user');
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+      // allowed XHR methods  
+      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+      res.send();
+  });
+});
+
+const data=require('./src/routes/user');
 // parse application/json
 app.use(bodyParser.json());
-app.use('/',dataEngineer);
+app.use('/',data);
 
 
 app.listen(4000, () => {
